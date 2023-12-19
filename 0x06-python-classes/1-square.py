f5d24e9e98cf2155eadbc5180086bee5e54f4b99
Anphimens <philipinamensah19@gmail.com>
2023-12-19 21:27:17 +0000

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,7 +8,7 @@ class Square:
     """ A class with name Square """
     def __init__(self, size):
         """ Initialization of the size """
-        self.__size__ = size
+        self.__size = size
         """ size is made private, because a size
         of a square is crucial for a square, many things depend of it
         """
