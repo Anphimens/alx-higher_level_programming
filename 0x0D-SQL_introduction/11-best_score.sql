3876c296a835f47e44bc6dadea93f7b2feb521f5
Anphimens <philipinamensah19@gmail.com>
2024-02-15 02:53:10 -0800

    lists all records of the second_table

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- Lists all records in the second_table with score >= 10
+-- Records are ordered by descending score
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC
+
